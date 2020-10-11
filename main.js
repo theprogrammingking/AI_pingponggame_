a06fcfb61ef66293de2e5874479bc6af749fdcef
ChiragKumar-09 <71137952+ChiragKumar-09@users.noreply.github.com>
2020-10-11 21:58:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 
+gameStatus = "";
+
+function startGame() {
+  gameStatus = "start";
+  document.getElementById("status").innerHTML = "Game Is Loaded";
+}
+
 // Right Wrsts variables
 rightwristX = 0;
 rightwristY = 0;
@@ -53,85 +60,85 @@ function modalLoaded() {
 }
 
 function draw(){
-
+if(gameStatus == "start") {
   image(video , 0 , 0 , 700 , 600);
 
- if (right_score > 0.2) {
-   fill(0, 0, 128);
-   stroke(0, 0, 128);
-   circle(rightwristX , rightwristY , 15);
+  if (right_score > 0.2) {
+    fill(0, 0, 128);
+    stroke(0, 0, 128);
+    circle(rightwristX , rightwristY , 15);
+  }
+ 
+  fill("black");
+  stroke("black");
+  rect(680,0,20,700);
+ 
+  fill("black");
+  stroke("black");
+  rect(0,0,20,700);
+  
+    //funtion paddleInCanvas call 
+    paddleInCanvas();
+  
+    //left paddle
+    fill(250,0,0);
+     stroke(0,0,250);
+     strokeWeight(0.5);
+    paddle1Y = mouseY; 
+    rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+    
+    
+     //pc computer paddle
+     fill("#FFA500");
+     stroke("#FFA500");
+    var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+     
+     //function midline call
+     midline();
+     
+     //funtion drawScore call 
+    drawScore();
+    
+    //function models call  
+    models();
+    
+    //function move call which in very important
+     move();
+     //function reset when ball does notcame in the contact of padde
+    function reset(){
+     ball.x = width/2+100,
+     ball.y = height/2+100;
+     ball.dx=3;
+     ball.dy =3;
+     
+     }
+
+
+   //function midline draw a line in center
+   function midline(){
+      for(i=0;i<480;i+=10) {
+      var y = 0;
+      fill("white");
+      stroke(0);
+      rect(width/2,y+i,10,480);
+      }
+   }
+   
+   
+   //function drawScore show scores
+   function drawScore(){
+      textAlign(CENTER);
+      textSize(20);
+      fill("white");
+      stroke(250,0,0)
+      text("Player:",100,50)
+      text(playerscore,140,50);
+      text("Computer:",500,50)
+      text(pcscore,555,50)
+   }
+
  }
-
- fill("black");
- stroke("black");
- rect(680,0,20,700);
-
- fill("black");
- stroke("black");
- rect(0,0,20,700);
  
-   //funtion paddleInCanvas call 
-   paddleInCanvas();
- 
-   //left paddle
-   fill(250,0,0);
-    stroke(0,0,250);
-    strokeWeight(0.5);
-   paddle1Y = mouseY; 
-   rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
-   
-   
-    //pc computer paddle
-    fill("#FFA500");
-    stroke("#FFA500");
-   var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
-    
-    //function midline call
-    midline();
-    
-    //funtion drawScore call 
-   drawScore();
-   
-   //function models call  
-   models();
-   
-   //function move call which in very important
-    move();
-}
-
-
-
-//function reset when ball does notcame in the contact of padde
-function reset(){
-   ball.x = width/2+100,
-   ball.y = height/2+100;
-   ball.dx=3;
-   ball.dy =3;
-   
-}
-
-
-//function midline draw a line in center
-function midline(){
-    for(i=0;i<480;i+=10) {
-    var y = 0;
-    fill("white");
-    stroke(0);
-    rect(width/2,y+i,10,480);
-    }
-}
-
-
-//function drawScore show scores
-function drawScore(){
-    textAlign(CENTER);
-    textSize(20);
-    fill("white");
-    stroke(250,0,0)
-    text("Player:",100,50)
-    text(playerscore,140,50);
-    text("Computer:",500,50)
-    text(pcscore,555,50)
 }
 
 
